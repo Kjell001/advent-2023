819070a33ead836523bfcb21edbc4c4fb1332a8c
Kjell Boeije <kjellboeije@gmail.com>
2023-12-08 10:44:21 +0100

    Day 8

---
+++ day-08.py
@@ -0,0 +1,23 @@
+from math import lcm
+
+def traverse(start, goals):
+	i = 0
+	node = start
+	while node not in goals:
+		node = map[node][dir[i % len(dir)]]
+		i += 1
+	return i
+
+with open('input-08.txt') as file:
+	dir = [s == 'R' for s in next(file).strip()]
+	next
+	map = {line[:3]: (line[7:10], line[12:15]) for line in file}
+
+starts = [node for node in map.keys() if node[2] == 'A']
+goals = [node for node in map.keys() if node[2] == 'Z']
+
+steps = [traverse(start, goals) for start in starts]
+
+print(f'Part 1: {traverse("AAA", ["ZZZ"])}')
+print(f'Part 2: {lcm(*[traverse(start, goals) for start in starts])}')
+
