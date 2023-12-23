a76329b4a19b42711b651ab9a11ff91cd93f6eda
Kjell Boeije <kjellboeije@gmail.com>
2023-12-24 00:29:40 +0100

    Day 18 part 2

--- day-18.py
+++ day-18.py
@@ -1,30 +1,23 @@
 import re
 
-pattern = re.compile(r'(.) (\d+) \(#(.+)(.)\)')
+pattern = re.compile(r'(.) (\d+) \(#(.+)(\)\)')
 dirs = {'R': 1, 'D': 1j, 'L': -1, 'U': -1j}
-with open('input-18-ex.txt') as file:
-	plan = [(dirs[d1], int(s1), [*dirs.values()][int(d2)], int(s2, 16)) for d1, s1, s2, d2 in [pattern.findall(line)[0] for line in file]]
+
+with open('input-18.txt') as file:
+	plan = [
+		(dirs[d1], int(s1), [*dirs.values()][int(d2)], int(s2, 16))
+		for d1, s1, s2, d2 in [pattern.findall(line)[0] for line in file]
+	]
 
 def get_area(plan):
-	# Perimeter
-	start = 0
-	neg, mid, pos = set(), set(), set()
+	area, perimeter, a = 0, 0, 0
 	for dir, steps in plan:
-		seg = [start + dir * i for i in range(steps + 1)]
-		neg |= {coord + dir * -1j for coord in seg}
-		mid |= {coord             for coord in seg}
-		pos |= {coord + dir *  1j for coord in seg}
-		start = seg[-1]
-	# Fill
-	open = pos if len(pos := pos - mid) < len(neg := neg - mid) else neg
-	area = set()
-	while open:
-		coord = open.pop()
-		if coord not in mid and coord not in area:
-			area.add(coord)
-			open |= {coord + dir for dir in dirs.values()}
-	return len(mid | area)
+		b = a + steps * dir
+		area += (b - a).real * a.imag # Shoelace formula
+		perimeter += abs(b - a)
+		a = b
+	return int(abs(area) + perimeter / 2 + 1) # Account for half perimeter an corners
 
 print(f'Part 1: {get_area(((d, s) for d, s, _, _ in plan))}')
-#print(f'Part 2: {get_area(((d, s) for _, _, d, s in plan))}')
+print(f'Part 2: {get_area(((d, s) for _, _, d, s in plan))}')
 
